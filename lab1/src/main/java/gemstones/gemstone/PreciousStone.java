87827cc495b5f4b680d3ab0d3e706098d39919b6
Valeria <valeriachelushkina@gmail.com>
2025-12-05 22:10:29 +0200

    add inherited classes for Precious and SemiPrecious Stones.

---
+++ lab1/src/main/java/gemstones/gemstone/PreciousStone.java
@@ -0,0 +1,29 @@
+package gemstones.gemstone;
+
+// class for representing Precious Stones
+// inherits Gemstone class
+public class PreciousStone extends Gemstone {
+
+    private String origin;
+
+    // constructor
+    public PreciousStone(String name, double weight, double cost, double transparency, String color, String origin) {
+        super(name, weight, cost, transparency, color);
+        this.origin = origin;
+    }
+
+    // getter for precious stone origin
+    public String getOrigin() {
+        return origin;
+    }
+
+    // setter for precious stone origin
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(", origin = %s [Precious]", origin);
+    }
+}
---
+++ lab1/src/main/java/gemstones/gemstone/SemiPreciousStone.java
@@ -0,0 +1,29 @@
+package gemstones.gemstone;
+
+// class for representing Semi Precious Stones
+// inherits Gemstone class
+public class SemiPreciousStone extends Gemstone {
+
+    private String hardness; // Mohs hardness scale
+
+    // constructor
+    public SemiPreciousStone(String name, double weight, double cost, double transparency, String color, String hardness) {
+        super(name, weight, cost, transparency, color);
+        this.hardness = hardness;
+    }
+
+    // getter for semiprecious stone hardness
+    public String getHardness() {
+        return hardness;
+    }
+
+    // setter for semiprecious stone hardness
+    public void setHardness(String hardness) {
+        this.hardness = hardness;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(", hardness = %s [Semiprecious]", hardness);
+    }
+}
